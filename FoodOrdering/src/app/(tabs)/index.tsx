008449712d77b720f5cdd954e1f43276b007d843
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-03-02 01:35:58 +0300

    Setting up the UI

--- FoodOrdering/src/app/(tabs)/index.tsx
+++ FoodOrdering/src/app/(tabs)/index.tsx
@@ -1,31 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from "react-native";
+import { Text, View } from "../../components/Themed";
+import products from "../../../assets/data/products";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+const product = products[1];
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Brenda</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View className="bg-white p-[10px] rounded-[20px]">
+      <Image source={{ uri: product.image }} className="w-full aspect-square" />
+      <Text className="text-[30px] font-[600] my-[10px]">{product.name}</Text>
+      <Text className="text-[#32cd32]">${product.price}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+const styles = StyleSheet.create({});
