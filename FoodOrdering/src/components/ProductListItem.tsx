f22c5a5e903ef155177ea584bf0ad47c197eeb74
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-03-02 11:16:02 +0300

    Update ProductListItem.tsx

--- FoodOrdering/src/components/ProductListItem.tsx
+++ FoodOrdering/src/components/ProductListItem.tsx
@@ -1,4 +1,4 @@
-import { Image, Pressable } from "react-native";
+import { Image, Pressable, StyleSheet } from "react-native";
 import { Text, View } from "../components/Themed";
 import { Product } from "../types";
 import { Link } from "expo-router";
@@ -13,12 +13,19 @@ type ProductListItemProps = {
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <Link href={`/menu/${product.id}`} asChild>
-      <Pressable onPress={()=>{}} className="flex-1 bg-white p-[10px] rounded-[20px] m-2">
+      <Pressable
+        onPress={() => {}}
+        className="flex-1 bg-white p-[10px] rounded-[20px] mt-2"
+        style={styles.container}
+      >
         <Image
           source={{ uri: product.image || defaultPizzaImage }}
           className="w-full aspect-square"
+          style={styles.image}
         />
-        <Text className="text-[18px] font-[600] my-[10px]">{product.name}</Text>
+        <Text className="text-[18px] font-[600] my-[10px]" style={styles.title}>
+          {product.name}
+        </Text>
         <Text className="text-[#32cd32]">${product.price}</Text>
       </Pressable>
     </Link>
@@ -26,3 +33,19 @@ const ProductListItem = ({ product }: ProductListItemProps) => {
 };
 
 export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    borderRadius: 20,
+    margin: 10,
+    marginVertical: 10
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  title: { fontSize: 18, fontWeight: "600", marginVertical: 10 },
+  price: {},
+});
