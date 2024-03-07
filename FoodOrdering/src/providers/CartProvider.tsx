c73ba3f3a4730b0e39289c16178fa91fc7e9c565
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-03-07 19:48:59 +0300

    Create CartProvider.tsx

---
+++ FoodOrdering/src/providers/CartProvider.tsx
@@ -0,0 +1,44 @@
+import React, {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useState,
+} from "react";
+import { CartItem, Product } from "@/types";
+
+type CartType = {
+  items:CartItem[];
+  addItem: (product: Product, size: CartItem["size"]) => void;
+};
+
+const CartContext = createContext<CartType>({
+  items: [],
+  addItem: () => {},
+});
+
+const CartProvider = ({ children }: PropsWithChildren) => {
+  const [items, setItems] = useState<CartItem[]>([]);
+
+  const addItem = (product: Product, size: CartItem["size"]) => {
+    // If already in Cart, increment quantity
+    const newCartItem: CartItem = {
+      id: "1", //generate
+      product,
+      product_id: product.id,
+      size,
+      quantity: 1,
+    };
+    setItems([newCartItem,...items]);
+  };
+
+  //    update quantity
+
+  return (
+    <CartContext.Provider value={{ items, addItem }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export default CartProvider;
+export const useCart = () => useContext(CartContext);
